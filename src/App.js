33a89a148f4b3b3ff1147fc60309648dfce6cb1a
FelipeSilva99 <felipe.rl.silva@hotmail.com>
2021-06-15 19:45:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component{
+  state = {
+    cont:0
+  }
+  
+  positive = () => {
+    if (this.state.cont < 10){
+      this.setState({
+        cont: this.state.cont + 1
+      })
+    }
+  }
+  
+  negative = () => {
+    if(this.state.cont > 0) {
+      this.setState({
+        cont: this.state.cont - 1
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.positive}>+</button>
+        <p>{this.state.cont}</p>
+        <button onClick={this.negative}>-</button>
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
